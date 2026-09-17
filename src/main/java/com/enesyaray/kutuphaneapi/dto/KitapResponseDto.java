ec65147fa4870a5faa7088471633c5eafa4e5241
enes <enesyry44@gmail.com>
2026-09-17 11:02:32 +0300

    17 eylül en son çalışan sürüm saat 11

--- src/main/java/com/enesyaray/kutuphaneapi/dto/KitapResponseDto.java
+++ src/main/java/com/enesyaray/kutuphaneapi/dto/KitapResponseDto.java
@@ -1,5 +1,14 @@
 package com.enesyaray.kutuphaneapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class KitapResponseDto {
     private Long id;
     private String isbn;
